38df618a86ef6074aa51f0aead36cd3dbd7eac98
DavidGuerrero <guerrero.david2@gmail.com>
2017-03-15 18:13:48 -0700

    Updates

--- PRIMARY_Backup_v2/PRIMARY_Backup_v2.py
+++ PRIMARY_Backup_v2/PRIMARY_Backup_v2.py
@@ -43,13 +43,36 @@ class Application(tk.Frame):
 		des = fileDialog.askdirectory()
 		print(des)
 		
+		
+	def initializeFolders(self):
+		
+		global alphabet
+		global folderName
+		
+		alphabet = []
+		folderName = []
+		
+		f = 65
+		
+		while f < 91:
+			alphabet.append(chr(f))
+			folderName.append(str(chr(f)) + "-Clients")
+			
+			f += 1
+		
+		#print(alphabet)
+		#print(folderName)
+		
+		
 	# Starts the file copy process	
 	def copyStart(self):
+		
+		#create folder arrays
+		self.initializeFolders()
 		
 		# Populate array with all the paths in selected source directory
 		src_files = os.listdir(src)
 				
-		
 		# Initialize variables for progress tracking
 		i = 0
 		e = len(src_files)
@@ -57,26 +80,48 @@ class Application(tk.Frame):
 		# Start for loop with every path in array
 		for file_name in src_files:
 			
+			#used to move though alphabet
+			j = 0
+				
+			myFolder = ""
+					
+			while j < 26:
+				if file_name[0] == alphabet[j]:
+					print (file_name + " belongs in the " + folderName[j] + " folder")
+					myFolder = folderName[j]
+				j += 1
+				
+			print (myFolder)
+			
 			# The full path is the concatination of the file name and the source path
 			file_name_src = os.path.join(src, file_name)
 			
 			# Print the full path of source
 			print(file_name_src)
-			
-			# The full path is the concatination of the file name and the source path
-			file_name_des = os.path.join(des, file_name)
-			
+
+			if myFolder != "":
+				file_name_client = os.path.join(des, myFolder)
+				file_name_des = os.path.join(file_name_client, file_name)
+				
+			else:
+				# The full path is the concatination of the file name and the source path
+				file_name_client = des
+				file_name_des = os.path.join(file_name_client, file_name)
+				
+
 			# Print the full path of destination
 			print(file_name_des)
-			
 			
 			if os.path.exists(file_name_des) != True:
 			
 				# If the path is a file
 				if (os.path.isfile(file_name_src)):
 					
+					if os.path.exists(file_name_client) != True:
+						os.makedirs(file_name_client)
+					
 					# Copy the file by itself
-					shutil.copy2(file_name_src, des)
+					shutil.copy2(file_name_src, file_name_des)
 					
 				# If the path is a directory
 				elif (os.path.isdir(file_name_src)):
@@ -84,12 +129,11 @@ class Application(tk.Frame):
 					# Copy the full tree
 					shutil.copytree(file_name_src, file_name_des)
 					
-					
 				# Add 1 to progress	
 				i += 1	
 				
 				# Calculate and Report progress to user	
-				percentComplete = str("{:10.2f}".format(i / e * 100) + "%" + " Complete - File Copied")
+				percentComplete = str("{:10.2f}".format(i / e * 100) + "%" + " Complete - File Copied \n")
 				print(percentComplete)
 			
 			else:
@@ -97,7 +141,7 @@ class Application(tk.Frame):
 				# Add 1 to progress	
 				i += 1	
 				
-				percentComplete = str("{:10.2f}".format(i / e * 100) + "%" + " Complete - File Already Exists")
+				percentComplete = str("{:10.2f}".format(i / e * 100) + "%" + " Complete - File Already Exists \n")
 				print(percentComplete)
 				
 root = tk.Tk()
