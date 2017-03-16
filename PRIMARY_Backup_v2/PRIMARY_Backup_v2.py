f5d0633812d29907afe8596e8c47ca0175033f26
DavidGuerrero <guerrero.david2@gmail.com>
2017-03-16 15:44:42 -0700

    updates

--- PRIMARY_Backup_v2/PRIMARY_Backup_v2.py
+++ PRIMARY_Backup_v2/PRIMARY_Backup_v2.py
@@ -1,14 +1,14 @@
 import tkinter as tk
 import tkinter.filedialog as fileDialog
 import tkinter.messagebox as messageBox
-import os
-import shutil
+import shlex, subprocess, shutil, os, platform, shutil2, posixpath, macpath
 
 class Application(tk.Frame):
 	def __init__(self, master=None):
 		tk.Frame.__init__(self, master)
 		self.pack()
 		self.createWidgets()
+		self.checkSystem()
 	
 	# Create 4 form buttons
 	def createWidgets(self):
@@ -30,7 +30,64 @@ class Application(tk.Frame):
 		# Button to quit program
 		self.Quit = tk.Button(self, text="Quit", fg="red", command=root.destroy)
 		self.Quit.pack(side="left")
+		
+	def isTagInstalled(self):
+		try:
+			subprocess.run("tag -v", shell=True, check=True)
+			return True
+		except Exception:
+			return False
+		
+	def installTag(self, tagFolder):
+		try:
+			print("cd " + tagFolder)
+			subprocess.run("cd " + tagFolder, "pwd", "make && sudo make install", shell=True, check=True)
+			print("TAG SUCCESFULLY INSTALLED")
+			return True
+		except Exception:
+			print("WARNING: COULD NOT INSTALL TAG \n \t COLOR TAGS WILL NOT COPY OVER")
+			return False
+		
 
+	def checkSystem(self):
+		
+		# Check the system version to know if color tags could be copied using TAG
+		systemVersion = platform.system()
+		
+		# Mac
+		if systemVersion == "Darwin":
+			
+			global checkTag
+			
+			# Get status of tag installation
+			checkTag = self.isTagInstalled()
+			
+			# If tag is not installed, install it from downloaded package in root folder.
+			if checkTag != True:
+				
+				# Get file directory of script
+				fileDir = os.path.dirname(os.path.realpath(__file__))
+				
+				# Get parent folder of script
+				rootDir = os.path.split(fileDir)
+				
+				# Join root path with TAG folder
+				tagDir = os.path.join(rootDir[0],"TAG")
+				
+				# Call function to install tag from tagDir folder
+				self.installTag(tagDir)
+				
+			else:
+				
+				print("TAG IS INSTALLED")
+				print("COLOR TAG COPYING SUPPORTED")
+				
+		# Other Systems
+		else:
+			
+			# Warn user about not being able to copy that data
+			print("WARNING: Copying Mac files from this system will not transfer over COLOR TAGS")
+		
 	# Initilizes global variable and stores user selected file path as the source
 	def selectSource_folder(self):
 		global src
@@ -123,13 +180,13 @@ class Application(tk.Frame):
 						os.makedirs(file_name_client)
 					
 					# Copy the file by itself
-					shutil.copy2(file_name_src, file_name_des)
+					shutil2.copy2(file_name_src, file_name_des)
 					
 				# If the path is a directory
 				elif (os.path.isdir(file_name_src)):
 					
 					# Copy the full tree
-					shutil.copytree(file_name_src, file_name_des)
+					shutil2.copytree(file_name_src, file_name_des, isTagInstalled=checkTag)
 					
 				# Add 1 to progress	
 				i += 1	
